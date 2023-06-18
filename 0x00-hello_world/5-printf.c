1d9ade913dcc3598748d389e5d12b9643bc044dd
John Karson <casonj254@gmail.com>
2023-06-18 02:10:44 -0700

    awa

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts function
+ * main - A program that prints a lin that uses printf function
  * Return: 0  (success)
  */
 int main(void)
