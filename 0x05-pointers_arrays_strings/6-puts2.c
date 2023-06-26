68aa3b17848c5ea5775350b223404c9ba2591510
John Karson <casonj254@gmail.com>
2023-06-26 07:59:18 -0700

    start

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,19 +7,18 @@
  */
 
 void puts2(char *str)
-        
-{       
-        int i;
-        int j = 0;
-        
-        while (str[j] != '\0')
-	{
-		j++;
-	}
 
+{
+	int i;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+	j++;
+	}
 	for (i = 0; i < j; i += 2)
 	{
-	_putchar(str[i]);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
