635ff2a59df9c9cca279e924e6af4d453f9b3cd9
John Karson <casonj254@gmail.com>
2023-07-07 06:46:32 -0700

    shiko

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints programs name
+ * @argc: The number of command-line arguments
+ * @argv: An array of command-line argument strings
+ * Return: 0 (Success)
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	char *program_name = argv[0];
+	
+	printf("%s\n", program_name);
+	return (0);
+}
