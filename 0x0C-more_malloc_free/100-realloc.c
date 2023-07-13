bb49f8476afd1b3962316fd1a452b0940f117241
John Karson <casonj254@gmail.com>
2023-07-13 14:12:12 -0700

    anaweza kataa

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _realloc - Reallocates a memory block using malloc and free
+ * @ptr: Pointer to the memory previously allocated with malloc
+ * @old_size: Size of the allocated space for ptr
+ * @new_size: New size of the memory block
+ *
+ * Return: Pointer to the reallocated memory block
+ * If malloc fails, the function causes process termination with status 98
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+		{
+			printf("Memory allocation failed\n");
+			exit(98);
+		}
+		return (new_ptr);
+	}
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+	{
+		printf("Memory allocation failed\n");
+		exit(98);
+	}
+	memcpy(new_ptr, ptr, (old_size < new_size) ? old_size : new_size);
+	free(ptr);
+	return (new_ptr);
+}
