4b335fb7cc2dc07049d8d0d5af309743eb8fabfd
John Karson <casonj254@gmail.com>
2023-07-27 14:12:19 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+size_t print_list(const list_t *h)
+{
+	if (h)
+	{
+		if (h->str)
+			printf("[%d] %s%s",
+					h - > len, h - > str, "\n");
+		else
+			printf("[%d] %s%s", 0, "(nil)", "\n");
+		if (h - > next)
+		{
+			return (1 + print_list(h - > next));
+		}
+		return (1);
+	}
+	return (0);
+}
