894c5b2a699d27df4d88f6bc6d8121114a8463a7
John Karson <casonj254@gmail.com>
2023-08-01 18:39:17 -0700

    commit

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,26 +16,26 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (head == NULL || *head == NULL)
 		return (-1);
 	if (index == 0)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+	current = *head;
+	while (current != NULL)
+	{
+		if (count == index - 1)
 		{
-			temp = *head;
-			*head = (*head)->next;
+			if (current->next == NULL)
+				return (-1);
+			temp = current->next;
+			current->next = temp->next;
 			free(temp);
 			return (1);
 		}
-		current = *head;
-		while (current != NULL)
-		{
-			if (count == index - 1)
-			{
-				if (current->next == NULL)
-					return (-1);
-				temp = current->next;
-				current->next = temp->next;
-				free(temp);
-				return (1);
-			}
-			current = current->next;
-			count++;
-		}
-		return (1);
+		current = current->next;
+		count++;
+	}
+	return (1);
 }
