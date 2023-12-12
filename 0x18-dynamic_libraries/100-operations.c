5b25cbfa5f030c2ea3166be1e9827d03e7f258ea
John Karson <casonj254@gmail.com>
2023-12-11 19:48:54 -0500

    continue c

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+/**
+ * add - Adds two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The sum of a and b.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Subtracts two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of subtracting b from a.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Multiplies two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The product of a and b.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Divides two integers.
+ * @a: The dividend.
+ * @b: The divisor.
+ *
+ * Return: The result of dividing a by b.
+ */
+int div(int a, int b)
+{
+	if (b == 0)
+	{
+		fprintf(stderr, "Error: Division by zero\n");
+		return (0);
+	}
+	return (a / b);
+}
+
+/**
+ * mod - Computes the remainder of the division of two integers.
+ * @a: The dividend.
+ * @b: The divisor.
+ *
+ * Return: The remainder of the division of a by b.
+ */
+int mod(int a, int b)
+{
+	if (b == 0)
+	{
+		fprintf(stderr, "Error: Modulo by zero\n");
+		return (0);
+	}
+	return (a % b);
+}
